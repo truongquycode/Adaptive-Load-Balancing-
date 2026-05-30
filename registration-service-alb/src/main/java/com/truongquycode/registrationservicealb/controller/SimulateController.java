8e3ec55cd6c3a6ac8a14ad8ce910ab3e689736e3
truongquycode <quy5704quy@gmail.com>
2026-05-30 10:43:45 +0700

    fix simulate

--- registration-service-alb/src/main/java/com/truongquycode/registrationservicealb/controller/SimulateController.java
+++ registration-service-alb/src/main/java/com/truongquycode/registrationservicealb/controller/SimulateController.java
@@ -24,7 +24,7 @@ public class SimulateController {
 		long count = requestCount.incrementAndGet();
 
 		// JSON parse
-		burnCpu(2500);
+		burnCpu(7000);
 
 		// network
 		Thread.sleep(10 + random.nextInt(20));
@@ -47,7 +47,7 @@ public class SimulateController {
 		}
 
 		// business logic
-		burnCpu(3000);
+		burnCpu(9000);
 
 		return ResponseEntity.ok(String.format("Request #%d completed", count));
 	}
